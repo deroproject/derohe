01016fe83c3e2cbd76f0c426c4a46b71fc153a89
8lecramm <89603217+8lecramm@users.noreply.github.com>
2022-07-29 20:59:39 +0200

    Added rpc calls signdata and checksignature

---
+++ walletapi/rpcserver/rpc_sign_data.go
@@ -0,0 +1,57 @@
+// Copyright 2017-2021 DERO Project. All rights reserved.
+// Use of this source code in any form is governed by RESEARCH license.
+// license can be found in the LICENSE file.
+// GPG: 0F39 E425 8C65 3947 702A  8234 08B2 0360 A03A 9DE8
+//
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package rpcserver
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"runtime/debug"
+
+	"github.com/deroproject/derohe/rpc"
+)
+
+func SignData(ctx context.Context, p rpc.Sign_Data) (result rpc.Sign_Data_Result, err error) {
+	defer func() { // safety so if anything wrong happens, we return error
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic occured. stack trace %s", debug.Stack())
+		}
+	}()
+
+	if p.Data == "" {
+		return result, fmt.Errorf("Data field is empty")
+	}
+
+	var decData []byte
+
+	if p.Plain {
+		decData = []byte(p.Data)
+	} else {
+		decData, err = hex.DecodeString(p.Data)
+	}
+
+	if err != nil {
+		return result, fmt.Errorf("Error parsing data err %s", err)
+	}
+	length := len(decData)
+
+	w := fromContext(ctx)
+	output := w.wallet.SignData(decData[:length])
+	result.SignedData = hex.EncodeToString(output[:])
+
+	return result, nil
+}
