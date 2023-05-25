e3665f03703fdfd71b4d5a42dc97a3f000b236d3
8lecramm <89603217+8lecramm@users.noreply.github.com>
2023-05-25 23:09:36 +0200

    Wallet token support

--- walletapi/wallet.go
+++ walletapi/wallet.go
@@ -16,26 +16,25 @@
 
 package walletapi
 
-import "os"
-import "fmt"
-import "sort"
-import "sync"
-import "time"
-import "strings"
-import "math/big"
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+	"time"
 
-import "encoding/pem"
-import "encoding/binary"
-
-import "github.com/go-logr/logr"
-
-import "github.com/deroproject/derohe/rpc"
-import "github.com/deroproject/derohe/cryptography/crypto"
-import "github.com/deroproject/derohe/cryptography/bn256"
-
-import "github.com/deroproject/derohe/walletapi/mnemonics"
-import "github.com/deroproject/derohe/transaction"
+	"github.com/deroproject/derohe/cryptography/bn256"
+	"github.com/deroproject/derohe/cryptography/crypto"
+	"github.com/deroproject/derohe/rpc"
+	"github.com/deroproject/derohe/transaction"
+	"github.com/deroproject/derohe/walletapi/mnemonics"
+	"github.com/go-logr/logr"
+)
 
 //import "github.com/deroproject/derohe/blockchain/inputmaturity"
 
@@ -127,6 +126,19 @@ func (w *Wallet_Memory) InsertReplace(scid crypto.Hash, e rpc.Entry) {
 		w.account.EntriesNative = map[crypto.Hash][]rpc.Entry{}
 	}
 	w.account.EntriesNative[scid] = entries
+}
+
+func (w *Wallet_Memory) TokenAdd(scid crypto.Hash) (err error) {
+	w.Lock()
+	defer w.Unlock()
+
+	if _, ok := w.account.EntriesNative[scid]; !ok {
+		w.account.EntriesNative[scid] = []rpc.Entry{}
+	} else {
+		return fmt.Errorf("token already added")
+	}
+
+	return nil
 }
 
 // generate keys from using random numbers
